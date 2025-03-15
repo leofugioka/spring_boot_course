40386d9ec78fbf75af0c6524b2496e33c4bfbf79
Leandro Fugioka <77177218+leofugioka@users.noreply.github.com>
2025-03-15 14:41:22 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -16,6 +18,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    private Set<Product> products = new HashSet<Product>();
 
     public Category() {};
 
@@ -40,6 +44,11 @@ public class Category implements Serializable {
         this.name = name;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -52,4 +61,5 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hashCode(getId());
     }
+
 }
